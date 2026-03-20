c9e4e258453eab6630764ce897b47e8ddedc8e46
EduardoChapeco <36956811+EduardoChapeco@users.noreply.github.com>
2026-03-20 17:38:17 -0300

    migrate: src/slides/showcase/index.ts

---
+++ src/slides/showcase/index.ts
@@ -0,0 +1,35 @@
+import Slide01Cover from './Slide01Cover';
+import Slide02DJCareer from './Slide02DJCareer';
+import Slide03EventProduction from './Slide03EventProduction';
+import Slide04Pyrotechnics from './Slide04Pyrotechnics';
+import Slide05Credentials from './Slide05Credentials';
+import Slide06Contact from './Slide06Contact';
+import Slide07LogoGridEvents from './Slide07LogoGridEvents';
+import Slide08LogoGridPartners from './Slide08LogoGridPartners';
+import Slide09VideoCarousel from './Slide09VideoCarousel';
+import Slide10SoundLightSetup from './Slide10SoundLightSetup';
+import Slide11EffectsShowcase from './Slide11EffectsShowcase';
+import Slide12ExecutiveShowcase from './Slide12ExecutiveShowcase';
+import Slide13ContactFull from './Slide13ContactFull';
+import Slide14AnimatedGrid from './Slide14AnimatedGrid';
+import Slide15MediaShowcase from './Slide15MediaShowcase';
+import Slide16FullscreenSlider from './Slide16FullscreenSlider';
+
+export const showcaseSlides = [
+  { component: Slide01Cover, name: 'Capa', template: 'title' },
+  { component: Slide02DJCareer, name: 'Trajetória DJ', template: 'timeline' },
+  { component: Slide03EventProduction, name: 'Produção de Eventos', template: 'gallery' },
+  { component: Slide04Pyrotechnics, name: 'Efeitos & Pirotecnia', template: 'media' },
+  { component: Slide05Credentials, name: 'Credenciais', template: 'media' },
+  { component: Slide06Contact, name: 'Contato', template: 'cta' },
+  { component: Slide07LogoGridEvents, name: 'Logos Eventos', template: 'grid' },
+  { component: Slide08LogoGridPartners, name: 'DJs & Parceiros', template: 'grid' },
+  { component: Slide09VideoCarousel, name: 'Vídeos Eventos', template: 'media' },
+  { component: Slide10SoundLightSetup, name: 'Estrutura Som & Luz', template: 'media' },
+  { component: Slide11EffectsShowcase, name: 'Efeitos Especiais', template: 'media' },
+  { component: Slide12ExecutiveShowcase, name: 'Portfólio Visual', template: 'media' },
+  { component: Slide13ContactFull, name: 'Contato Completo', template: 'cta' },
+  { component: Slide14AnimatedGrid, name: 'Galeria Animada', template: 'grid' },
+  { component: Slide15MediaShowcase, name: 'Portfólio Completo', template: 'media' },
+  { component: Slide16FullscreenSlider, name: 'Slider Fullscreen', template: 'media' },
+];
